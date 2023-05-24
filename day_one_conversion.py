8599c9dbc57ba15599e0b672a57b9193fcf265cc
Mark Larson <10730050+mdlarson@users.noreply.github.com>
2023-05-24 18:03:26 -0400

    Update filepaths and heading sizes

--- day_one_conversion.py
+++ day_one_conversion.py
@@ -3,7 +3,7 @@
 import json
 
 # Read the JSON file
-with open('20201124-DayOne.json', 'r', encoding='utf8') as file:
+with open('DayOneArchive.json', 'r', encoding='utf8') as file:
     data = json.load(file)
 
 # Convert JSON entries to Markdown
@@ -12,19 +12,15 @@ MARKDOWN_CONTENT = ""
 for entry in data['entries']:
     title = entry['creationDate']
     content = entry['text']
-
-    # Check if 'photos' key exists
-    if 'photos' in entry:
-        photo_identifier = entry['photos'][0]['identifier']
-        main_entry = f"## {title}\n\n{content}\n\n![{photo_identifier}](dayone-moment://{photo_identifier})\n\n"
-    else:
-        main_entry = f"## {title}\n\n{content}\n\n"
+    
+    main_entry = f"# {title}\n\n\
+                    {content}\n\n"
 
     if 'weather' in entry:
         # convert C to F
         weather_temp = ((entry['weather']['temperatureCelsius'] * 9) / 5) + 32
         weather_description = entry['weather']['conditionsDescription']
-        weather_entry = f"# {weather_temp}°, {weather_description}\n\n"
+        weather_entry = f"### {weather_temp}°, {weather_description}\n\n"
     else:
         pass
 
@@ -32,5 +28,5 @@ for entry in data['entries']:
     MARKDOWN_CONTENT += weather_entry
 
 # Write Markdown content to a file
-with open('day-one-conversion/journal_entries.md', 'w', encoding='utf8') as file:
+with open('journal_entries.md', 'w', encoding='utf8') as file:
     file.write(MARKDOWN_CONTENT)
